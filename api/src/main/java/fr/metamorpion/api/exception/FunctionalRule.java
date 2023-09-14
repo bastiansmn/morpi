60045b534c37ea74007eebec8b7d7e6c000d33b8
ADAM Pauline <pauline92@hotmail.fr>
2023-09-14 15:27:03 +0200

    Changes by naysl

--- api/src/main/java/fr/metamorpion/api/exception/FunctionalRule.java
+++ api/src/main/java/fr/metamorpion/api/exception/FunctionalRule.java
@@ -11,6 +11,9 @@ public enum FunctionalRule {
     GAME_0003("ALREADY_IN_GAME", "Vous êtes déjà dans la partie"),
     GAME_0004("NOT_IN_GAME", "Le joueur n'appartient pas à la partie"),
     GAME_0005("MOVE_UNAUTHORIZED", "Impossible de jouer ce coup"),
+    GAME_0007("API_NOT_REACHABLE", "API non trouvée"),
+    REQUEST_0001("BAD_REQUEST", "La requête est mauvaise"),
+    SERVER_0001("INTERNAL_SERVER_ERROR", "Erreur interne du serveur")
     ;
 
     private final String name;
